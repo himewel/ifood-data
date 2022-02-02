417e8d36fbaabe18855425b57dd1995598eff8c6
Welbert Castro <himewel@users.noreply.github.com>
2022-02-02 10:24:46 -0300

    Fix multiple source rows issue

--- spark/src/order_items_datamart.py
+++ spark/src/order_items_datamart.py
@@ -3,14 +3,13 @@ from datetime import datetime
 from sys import argv
 
 from delta import DeltaTable
-from pyspark.sql.functions import col, concat, explode, lit, to_date
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col, concat, date_format, explode, lit, rank, to_date
 
 from utils import get_spark
 
 
-def transform(start_date, end_date):
-    spark = get_spark(app_name="order-items-trusted-datamart")
-
+def get_date_partition():
     date_partition = to_date(
         concat(
             col("year_partition"),
@@ -20,23 +19,48 @@ def transform(start_date, end_date):
             col("day_partition"),
         )
     )
+    return date_partition
+
+
+def get_last_order(df):
+    partition = Window.partitionBy(col("order_id")).orderBy(
+        col("order_created_at").desc()
+    )
+    df = (
+        df.withColumn("rank_value", rank().over(partition))
+        .where(col("rank_value") == 1)
+        .drop("rank_value")
+    )
+    return df
+
+
+def transform(start_date, end_date):
+    spark = get_spark(app_name="order-items-trusted-datamart")
 
     orders = (
         spark.read.format("delta")
         .load("s3a://ifood-lake/raw/orders")
-        .where(
-            (date_partition >= start_date.date()) & (date_partition < end_date.date())
-        )
-        .withColumn("items", explode("items"))
+        .where(get_date_partition() >= start_date.date())
+        .where(get_date_partition() < end_date.date())
+    )
+
+    orders = get_last_order(orders)
+
+    orders = (
+        orders.withColumn("item", explode("items"))
         .select(
             "order_id",
             "order_created_at",
-            "items.*",
-            "year_partition",
-            "month_partition",
-            "day_partition",
+            "item",
+            col("item.sequence").alias("sequence"),
+            date_format(col("order_created_at"), "yyyy").alias("year_partition"),
+            date_format(col("order_created_at"), "MM").alias("month_partition"),
+            date_format(col("order_created_at"), "dd").alias("day_partition"),
         )
+        .drop_duplicates(subset=["order_id", "sequence"])
+        .drop("sequence")
     )
+    orders.printSchema()
 
     trusted_path = "s3a://ifood-lake/trusted/order_items"
     if DeltaTable.isDeltaTable(spark, trusted_path):
@@ -45,7 +69,10 @@ def transform(start_date, end_date):
             trusted_df.alias("trusted")
             .merge(
                 source=orders.alias("orders"),
-                condition=col("trusted.order_id") == col("orders.order_id"),
+                condition=(
+                    (col("trusted.order_id") == col("orders.order_id"))
+                    & (col("trusted.item.sequence") == col("orders.item.sequence"))
+                ),
             )
             .whenMatchedUpdateAll()
             .whenNotMatchedInsertAll()
